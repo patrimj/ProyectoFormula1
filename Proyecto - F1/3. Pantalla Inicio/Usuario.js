dbfa7595ea981379f888eba1b81a42db4c5cf582
patrimj <patriimj1@gmail.com>
2023-10-27 11:53:35 +0200

    eliminadas clases para generales

--- Proyecto - F1/3. Pantalla Inicio/Usuario.js
+++
@@ -1,5 +0,0 @@
-//Se debe asignar un piloto titular y suplente a tu usuario
-import { Piloto } from "./clases.js";
-import { Usuario } from "./clases.js";
-
-const usuario = new Usuario('TuUsuario', new Piloto ('Nombre1', 'Apellido1', 'Español'), new Piloto ('Nombre2', 'Apellido2', 'Español'));
